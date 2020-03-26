82e454355548f03764fca822422e685d4a055ab4
lvl <mamad.za69@gmail.com>
2020-03-27 02:15:07 +0430

    some fix

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
-
 import './collection-item.styles.scss';
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className="collection-item">
-        <div className="image"
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        >
-        </div>
+import CustomButton from '../custom-button/custom-button.components';
 
-        <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
-        </div>
-    </div>
-)
+import {connect} from 'react-redux';
+import { addNewItemToCart } from '../../redux/cart/cart.actions';
 
-export default CollectionItem;
+const CollectionItem = ({item, addNewItemToCart}) => {
+    
+    const {imageUrl, name, price} = item;
+    return (
+        <div className="collection-item">
+            <div className="image"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            >
+            </div>
+
+            <div className="collection-footer">
+                <span className="name">{name}</span>
+                <span className="price">{price}</span>
+            </div>
+
+            <CustomButton onClick={() => addNewItemToCart(item)} inverted>Add to cart</CustomButton>
+        </div>
+    )
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    addNewItemToCart: (item) => dispatch(addNewItemToCart(item))
+})
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ import { CartActionTypes } from './cart.types';
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN,
 })
+
+export const addNewItemToCart = (item) => ({
+    type: CartActionTypes.ADD_NEW_ITEM,
+    payload: item
+})
